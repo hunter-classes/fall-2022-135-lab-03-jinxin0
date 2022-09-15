2ac5ceffa939b0512011d31e9ea3a4d799f2ad9c
Jinxin Lin <JINXIN.LIN07@myhunter.cuny.edu>
2022-09-15 15:30:43 -0400

    finished task A

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
-// add your code as appropriate 
 #include <iostream>
+#include "reservoir.h"
 
 int main()
 {
-  
-
-  
-  return 0;
+    std::cout << "East basin storage: " << get_east_storage("05/20/2018") << " billion gallons\n";
+    return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -1,1 +1,20 @@
-// add your code
+#include <climits>
+#include <fstream>
+#include <string>
+#include "reservoir.h"
+
+double get_east_storage(std::string d)
+{
+    std::ifstream in_file("Current_Reservoir_Levels.tsv");
+    std::string date;
+    double eastSt = -1;
+    in_file.ignore(INT_MAX, '\n'); 
+    while (in_file >> date >> eastSt) 
+    { 
+        if (date == d)
+            break;
+        in_file.ignore(INT_MAX, '\n'); 
+    }
+    in_file.close();
+    return eastSt;
+}
